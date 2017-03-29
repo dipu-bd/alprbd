fc8ca5ffdbdaebf61f388d47b57f49dffcd755a4
dipu-bd <dipu.sudipta@gmail.com>
2017-03-30 01:22:29 +0600

    Stage 14 completed

--- modules/ExtractOriginal.py
+++ modules/ExtractOriginal.py
@@ -12,27 +12,27 @@ def process(img, region1, region2):
     :param img: original plate image
     :param region1: position relative to scaled
     :param region2: position relative to region1
-    :return orignal plate image and region 
+    :return original plate image and region 
     """
 
-    # translate to intermediate region
+    # translate to scaled region
     x1, x2, y1, y2 = region2
-    x, y = region1[0], region1[1]
+    x = region1[0]
+    y = region1[2]
 
     x1 += x
     x2 += x
     y1 += y
     y2 += y
 
-    # translate points to original
+    # translate scaled to original
     row, col = img.shape
     height, width = cfg.SCALE_DIM
 
-    px, py = x1, y1
-    x1 = int((px + x1) * row / height)
-    x2 = int((px + x2) * row / height)
-    y1 = int((py + y1) * col / width)
-    y2 = int((py + y2) * col / width)
+    x1 = int(x1 * row / height)
+    x2 = int(x2 * row / height)
+    y1 = int(y1 * col / width)
+    y2 = int(y2 * col / width)
 
     # extract plate and regions
     plate = img[x1:x2, y1:y2]
@@ -50,25 +50,34 @@ def run(stage):
     """
     util.log("Stage", stage, "Crop the plate regions")
     for read in util.get_data(stage):
-        # open scaled region data
-        region1 = util.stage_data(read, 7)
-        region1 = np.load(region1)
+
         # open relative region data
         region2 = util.stage_data(read, stage)
         region2 = np.load(region2)
+
+        # open scaled region data
+        name = ".".join(read.split(".")[1:])
+        region1 = util.stage_data(name, 7)
+        region1 = np.load(region1)
+
         # get original image
-        img = util.stage_image(read, 1)
+        name = ".".join(read.split(".")[2:])
+        img = util.stage_image(name, 1)
         img = cv2.imread(img, cv2.CV_8UC1)
+
         # get result
         plate, region = process(img, region1, region2)
-        # save plates to image files
-        write = util.stage_image(read, stage + 1)
-        cv2.imwrite(write, plate)
+
         # save new region to data files
         write = util.stage_data(read, stage + 1)
         np.save(write, region)
+
+        # save plates to image files
+        write = util.stage_image(read, stage + 1)
+        cv2.imwrite(write, plate)
+
         # log
         util.log("Converted", read, stage=stage)
-    # end for
 
+    # end for
 # end function
