4c0ad442488ed38d0a9c892c95d50a7dfa0ec34e
dipu-bd <dipu.sudipta@gmail.com>
2017-03-29 07:09:14 +0600

    Main.py: changed adapting modular approach

--- Main.py
+++ Main.py
@@ -1,64 +1,77 @@
 #!/usr/bin/python
 
-import cv2
-from os import path
-import modules.util as util
-import modules.config as cfg 
-
-import Grayscale
-import Sobel
+import os
+import sys
+import alpr
+from modules import util
+from modules import config as cfg
 
 
-# Define working directory
-WORK_PATH = path.join(path.curdir, "stages")
-util.ensure_path(WORK_PATH)
-
-
-def openStage(stage = 0):
+def load_images(stage = 0):
     """Open an stage by stage number
     :param stage: Stage number to open 
     :return: An array of stage objects
     """
-    path.join(WORK_PATH,
-#end function
 
-def main(imgFile, stage):
+    # get folder
+    folder = os.path.join(cfg.WORK_PATH, str(stage))
 
-    for i in range(0, 2):
-        if(isinstance(x, (int)) && i != stage):
-            continue;
-        #end if
-        perform()
-           
-    #1 read image
-    img = cv2.imread(imgFile)
-    
-    ##### Stage 2 ##### 
-    #1 read original image
-    img = cv2.imread(imgFile)
-    #2 to grayscale        
-    gray = Grayscale.apply(original.image) 
-    stage1 = Stage('original', gray)
-    stage1.save();
-    
-    ##### Stage 2 #####
-    #1 rescale 
-    scaled = Rescale.apply(gray.img)
-    #2 apply sobel
-    sobel = Sobel.apply(scaled)
-    #3 apply gauss filter
-    gauss = Gauss.apply(sobel), tools.build_blur_kernel())
-    
-    
-    
+    # check folder
+    if not os.path.exists(folder):
+        util.log(folder, "does not exists")
+        raise Exception("Input folder does not exists")
+    # end if
 
+    # folder for next stage
+    folder2 = os.path.join(cfg.WORK_PATH, str(stage + 1))
+    util.ensure_path(folder2)
+
+    # Open all images
+    images = []
+    valid_images = ["jpg", "gif", "png", "bmp"]
+    for read in os.listdir(folder):
+        name, ext = util.split_file(read)
+        if ext in valid_images:
+            write = os.path.join(folder2, name+"."+ext)
+            images.append((read, write))
+        # end if
+    # end for
+
+    return images
+# end function
+
+
+def run_stage(stage_no):
+    images = load_images(stage_no)
+    alpr.execute(stage_no, images)
+# end function
+
+
+def main(*args):
+
+    if len(args) == 2:
+        # run specific stage
+        run_stage(int(args[1]))
+
+    elif len(args) == 1:
+        # run all stages sequentially
+        for key in alpr.STAGE_MAP:
+            run_stage(key)
+        # end for
+
+    else:
+        # wrong number of arguments
+        util.log("Invalid number of arguments")
+    # end if
+
+    print("\nSUCCESS.")
 # end main
 
-# delete if exists
-if os.path.exists(STAGE_PATH):
-    #call(["rm", "-rf", STAGE_PATH])
-#end if
 
+# ensure that working directory exists
+util.ensure_path(cfg.WORK_PATH)
+
+# start program
 if __name__ == "__main__":
-    main(sys.argv[2], sys.argv[3])
-
+    main(sys.argv)
+# end if
