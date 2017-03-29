3230bb6de56459edd282c7d4b1afb96e5d93d036
dipu-bd <dipu.sudipta@gmail.com>
2017-03-29 06:54:14 +0600

    alpr.py: passing proper args to execute()

--- alpr.py
+++ alpr.py
@@ -6,23 +6,23 @@ from modules import util
 STAGE_MAP = {}
 
 
-def execute(stage):
+def execute(id, images):
     """
     Call the function to for given stage variable.    
     :param stage: Must contain an ID and an images array.
     :return: True if success, False otherwise.
     """
     # check if stage is valid
-    if stage['id'] not in STAGE_MAP:
-        util.log("Unknown stage:", stage['id'])
+    if id not in STAGE_MAP:
+        util.log("Unknown stage:", id)
         return False
     # end if
 
     # execute the stage function for each image
-    f = STAGE_MAP[stage['id']]
-    for read, write in stage['images']:
+    f = STAGE_MAP[id]
+    for read, write in images:
         f(read, write)
-        util.log("Executed:", stage['id'], 'for', read, stage=stage['id'])
+        util.log("Executed:", id, stage=id)
     # end for
 
     return True
