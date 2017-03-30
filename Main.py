1e94da845d03143eb89a7fa7fbf8c4def756d93f
dipu-bd <dipu.sudipta@gmail.com>
2017-03-30 08:55:11 +0600

    Main: range stage input support

--- Main.py
+++ Main.py
@@ -8,12 +8,34 @@ def main(argv):
 
     stages = []
 
-    if len(argv) == 1:
+    if len(argv) == 1:      # no input given
         # all stages sequentially
         stages = range(1, len(alpr.STAGE_MAP) + 1)
 
-    elif len(argv) >= 2:
-        # specified stages in specific order
+    elif len(argv) == 2:    # either a range or single stage
+        try:
+            if ':' not in argv[1]:  # single stage
+                stages = [int(argv[1])]
+            else:                   # range of stages
+
+                start, stop = argv[1].split(':')
+                if len(start) == 0:
+                    start = 1
+                else:
+                    start = int(start)
+                # end if
+                if len(stop) == 0:
+                    stop = len(alpr.STAGE_MAP) + 1
+                else:
+                    stop = int(stop) + 1
+                # end if
+                stages = range(start, stop)
+            # end if
+        except err:
+            pass
+        # end try
+
+    elif len(argv) > 2:     # specified stages in specific order
         for i in range(1, len(argv)):
             try:
                 stages.append(int(argv[i]))
@@ -23,6 +45,7 @@ def main(argv):
         # end for
     # end if
 
+    # check if any input has been given
     if len(stages) == 0:
         # wrong number of arguments
         util.log("Invalid number of arguments")
