1b7452869d54051a1f5b8daa3f21aff4d6c897eb
dipu-bd <dipu.sudipta@gmail.com>
2017-06-17 09:16:00 +0600

    config added

---
+++ config.py
@@ -0,0 +1,14 @@
+#
+# Permitted numberals and lettes
+# Source: https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Bangladesh
+#
+
+# The letters permitted in the vehicle registration plate
+letters = u"অইউএকখগঘঙচছজঝতথঢডটঠদধনপফবভমযরলশসহ"
+
+# The numerals permitted in the vehicle registration plate
+numerals = u"০১২৩৪৫৬৭৮৯"
+
+
+# Image configuration
+size = (30, 30)
