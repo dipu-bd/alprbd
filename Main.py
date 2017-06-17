c69081e191489335905f1826ae4a8e6f84c54305
dipu-bd <dipu.sudipta@gmail.com>
2017-06-17 09:16:28 +0600

    testing using PIL image library

--- Main.py
+++ Main.py
@@ -1,4 +1,16 @@
 import os
-import cv2
+import numpy as np
+import config as cfg
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 
+if not os.path.exists('output'):
+  os.mkdir('output')
 
+mat = np.zeros(cfg.size, dtype=np.uint8)
+img = Image.fromarray(mat)
+draw = ImageDraw.Draw(img)
+font = ImageFont.truetype("fonts/solaimanlipi.ttf", 20)
+draw.text((5, 2), "A", 255, font=font)
+img.save('output/0001.jpg')
