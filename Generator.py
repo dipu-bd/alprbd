fec05c08f969d29644a46806445cd0bb654f466f
dipu-bd <dipu.sudipta@gmail.com>
2017-07-07 13:58:40 +0600

    formatting fixed

--- Generator.py
+++ Generator.py
@@ -16,24 +16,27 @@ FRAME = np.zeros([100, 300], dtype=np.uint8)
 OUTPUT_PATH = os.path.join('output', 'generated')
 
 
-def checkPath(output):
+def check_path(output):
+    """
+    if a directory does not exists, creates it.
+    """
     if not os.path.exists(output):
         os.makedirs(output)
     #end if
 # end function
 
 
-def trim(img):
+def trim_image(img):
     """
     Trims the image
     """
     img_arr = np.array(img)
     rows, cols = img_arr.shape
     nzx, nzy = np.nonzero(img_arr)
-    x1 = max(0, np.min(nzx) - 3)
-    x2 = min(rows, np.max(nzx) + 3)
-    y1 = max(0, np.min(nzy) - 3)
-    y2 = min(cols, np.max(nzy) + 3)
+    x1 = max(0, np.min(nzx))
+    x2 = min(rows, np.max(nzx))
+    y1 = max(0, np.min(nzy))
+    y2 = min(cols, np.max(nzy))
     cropped = img_arr[x1:x2, y1:y2]
     return Image.fromarray(cropped)
 # end function
@@ -45,11 +48,7 @@ def generate(data, font):
     """
     global INDEX
 
-    # define font
-    font_path, font_size = font
-    font = ImageFont.truetype(font_path, font_size)
-
-    for letter,label in data.items():
+    for letter, label in data.items():
         INDEX += 1
         # create a grayscale image
         img = Image.fromarray(FRAME)
@@ -57,11 +56,11 @@ def generate(data, font):
         draw = ImageDraw.Draw(img)
         draw.text((5, 5), letter, 255, font=font)
         # trim image
-        img = trim(img)
+        img = trim_image(img)
         # save image
         name = '{:05d}.png'.format(INDEX)
         save_to = os.path.join(OUTPUT_PATH, label)
-        checkPath(save_to)
+        check_path(save_to)
         img.save(os.path.join(save_to, name))
     # end for
 # end function
@@ -71,7 +70,8 @@ def run():
     """
     To generate the image from the texts
     """
-    for font in cfg.UNICODE_FONTS:
+    for font_path, font_size in cfg.UNICODE_FONTS:
+        font = ImageFont.truetype(font_path, font_size)
         generate(cfg.LETTER_LABELS, font)
         generate(cfg.NUMERAL_LABELS, font)
     # end for
