3c5087d7c9924cc6a3215d7b75dd8408492a4719
dipu-bd <dipu.sudipta@gmail.com>
2017-07-07 13:30:02 +0600

    assigned labels to letters and numerals

--- config.py
+++ config.py
@@ -2,45 +2,67 @@
 # Source: https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Bangladesh
 letters = u"অইউএকখগঘঙচছজঝতথঢডটঠদধনপফবভমযরলশসহ"
 
+letter_lables = {
+    u"অ": "O",
+    u"ই": "E",
+    u"উ": "U",
+    u"এ": "A",
+    u"ক": "K",
+    u"খ": "Kha",
+    u"গ": "G",
+    u"ঘ": "Gha",
+    u"ঙ": "Uo",
+    u"চ": "C",
+    u"ছ": "Cha",
+    u"জ": "J",
+    u"ঝ": "Jha",
+    u"ত": "Ta",
+    u"থ": "Tha",
+    u"ঢ": "Do",
+    u"ড": "Dho",
+    u"ট": "To",
+    u"ঠ": "Tho",
+    u"দ": "Da",
+    u"ধ": "Dha",
+    u"ন": "N",
+    u"প": "P",
+    u"ফ": "F",
+    u"ব": "B",
+    u"ভ": "V",
+    u"ম": "M",
+    u"য": "Z",
+    u"র": "R",
+    u"ল": "L",
+    u"শ": "Sh",
+    u"স": "S",
+    u"হ": "H"
+}
+
 # The numerals permitted in the vehicle registration plate
 # Source: https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Bangladesh
 numerals = u"০১২৩৪৫৬৭৮৯"
 
-# Strings - Bijoy Format
-strings = [
-  u'XvKv',
-  u'PÆ',
-  u'‡g‡U«v',
-  u'Lyjbv',
-  u'h‡kvi',
-  #u'ewikvj',
-  #u'iscyi',
-  #u'ivRkvnx',
-  #u'w`bvRcyi'
-]
-
-"""
-strings = [
-  u'ঢাকা',
-  u'চট্ট',
-  u'মেট্রো',
-  u'খুলনা',
-  u'যশোর',
-  #u'বরিশাল',
-  #u'রংপুর',
-  #u'রাজশাহী',
-  #u'দিনাজপুর'
-]
-"""
+numeral_labels = {
+    u"০": "0",
+    u"১": "1",
+    u"২": "2",
+    u"৩": "3",
+    u"৪": "4",
+    u"৫": "5",
+    u"৬": "6",
+    u"৭": "7",
+    u"৮": "8",
+    u"৯": "9"
+}
 
 # fonts - [(location, size)]
 unicode_fonts = [
-  #("fonts/bangla.ttf", 72),
-  ("fonts/siyamrupali.ttf", 38),
-  ("fonts/solaimanlipi.ttf", 46),
-  ("fonts/sutonnyomj.ttf", 48)
+    #("fonts/bangla.ttf", 72),
+    ("fonts/siyamrupali.ttf", 38),
+    ("fonts/solaimanlipi.ttf", 46),
+    ("fonts/sutonnyomj.ttf", 48)
 ]
 
 bijoy_fonts = [
-  ("fonts/sutonnymj.ttf", 48),
+    ("fonts/sutonnymj.ttf", 48),
 ]
