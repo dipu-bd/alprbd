8afc5d4e1178e0535b19415a1d767c781a2b0a19
dipu-bd <dipu.sudipta@gmail.com>
2017-07-08 11:01:35 +0600

    modified samples a little

--- digit_test.py
+++ digit_test.py
@@ -12,6 +12,27 @@ def read_images(folder):
     return np.sort([file for file in glob(folder + '**/*.*', recursive=True)])
 # end if
 
+def trim_image(img_file):
+    """
+    Trims the image
+    """
+    # open
+    img = cv2.imread(img_file, 0)
+    rows, cols = img.shape
+    # find area
+    nzx, nzy = np.nonzero(img)
+    x1 = max(0, np.min(nzx))
+    x2 = min(rows, np.max(nzx) + 2)
+    y1 = max(0, np.min(nzy))
+    y2 = min(cols, np.max(nzy) + 2)
+    # crop
+    cropped = img[x1:x2, y1:y2]
+    # resize
+    resized = cv2.resize(cropped, cfg.IMAGE_DIM)    
+    # save
+    cv2.imwrite(img_file, resized)
+# end function
+
 def main():
     """Main function"""
     if not os.path.exists(cfg.DIGIT_SAMPLES):
@@ -23,6 +44,7 @@ def main():
     
     files = read_images(cfg.DIGIT_SAMPLES)
     for file in files:
+        #trim_image(file)
         image = cv2.imread(file, 0)
         image = cv2.resize(image, (28, 28))
         X = np.reshape(image, (1, 784))
--- letter_test.py
+++ letter_test.py
@@ -12,6 +12,27 @@ def read_images(folder):
     return np.sort([file for file in glob(folder + '**/*.*', recursive=True)])
 # end if
 
+def trim_image(img_file):
+    """
+    Trims the image
+    """
+    # open
+    img = cv2.imread(img_file, 0)
+    rows, cols = img.shape
+    # find area
+    nzx, nzy = np.nonzero(img)
+    x1 = max(0, np.min(nzx))
+    x2 = min(rows, np.max(nzx) + 2)
+    y1 = max(0, np.min(nzy))
+    y2 = min(cols, np.max(nzy) + 2)
+    # crop
+    cropped = img[x1:x2, y1:y2]
+    # resize
+    resized = cv2.resize(cropped, cfg.IMAGE_DIM)    
+    # save
+    cv2.imwrite(img_file, resized)
+# end function
+
 def main():
     """Main function"""
     if not os.path.exists(cfg.LETTER_SAMPLES):
@@ -23,7 +44,8 @@ def main():
     
     files = read_images(cfg.LETTER_SAMPLES)
     for file in files:
-        image = cv2.imread(file, 0)
+        #trim_image(file)
+        image = cv2.imread(file, 0)        
         image = cv2.resize(image, (28, 28))
         X = np.reshape(image, (1, 784))
         Y = np.matmul(X, W) + B
