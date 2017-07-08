b54cd59c170f2deae42ca142329ca5b2f535375d
dipu-bd <dipu.sudipta@gmail.com>
2017-07-08 17:04:18 +0600

    combined digit and letter classifier

--- digits_softmax.py
+++ digits_softmax.py
@@ -1,78 +1,23 @@
 """
-A very simple DIGIT classifier.
-See extensive documentation at
-http://tensorflow.org/tutorials/mnist/beginners/index.md
+DIGIT classifier
 """
-import os
-import numpy as np
-import tensorflow as tf
-from utils import get_digit_data
 import config as cfg
+from utils import get_digit_data
+from softmax_1layer import train
 
 # Import data
-mnist = get_digit_data()
-
-batch_size = 100
-image_dim = 28 * 28
-learning_rate = 0.3
-num_classes = len(cfg.NUMERALS)
-
-# Create the model
-X = tf.placeholder(tf.float32, [None, image_dim])
-W = tf.Variable(tf.zeros([image_dim, num_classes]))
-B = tf.Variable(tf.zeros([num_classes]))
-Y = tf.matmul(X, W) + B
-
-# Define loss and optimizer
-Y_ = tf.placeholder(tf.float32, [None, num_classes])
-
-# The formulation of cross-entropy
-cross_entropy = tf.reduce_mean(
-    tf.nn.softmax_cross_entropy_with_logits(labels=Y_, logits=Y))
-
-train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
-
-# Accuracy measure
-correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-# Init session
-sess = tf.InteractiveSession()
-tf.global_variables_initializer().run()
-
-def train_model(i):
-    batch_X, batch_Y = mnist.train.next_batch(batch_size)
-    sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y})
-
-    if i % 1000 == 0:
-        a, c = sess.run([accuracy, cross_entropy],
-                        feed_dict={X: mnist.train.images, Y_: mnist.train.labels})
-        print('Train', i, '|', 'accuracy:', a, 'loss:', c)
-    # end if
-# end function
-
-def main(_):
-    # Train
-    print()
-    for i in range(10000):
-        train_model(i)
-    # end for
-
-    # Test
-    print()
-    a, c = sess.run([accuracy, cross_entropy],
-                    feed_dict={X: mnist.test.images, Y_: mnist.test.labels})
-    print('Test', '|', 'accuracy:', a, 'loss:', c)
-
-    # save trained model
-    if not os.path.exists('output'):
-        os.makedirs('output')
-    # end if
-    weight, base = sess.run([W, B])
-    np.save(cfg.DIGIT_WEIGHTS, weight)
-    np.save(cfg.DIGIT_BASES, base)
+def main():
+    """Main function"""
+    train(get_digit_data(),
+          input_dim=28*28,
+          output_dim=len(cfg.NUMERALS),
+          learning_rate=0.3,
+          iterations=5000,
+          batch_size=100,
+          base_file=cfg.DIGIT_BASES,
+          weights_file=cfg.DIGIT_WEIGHTS)
 # end function
 
 if __name__ == '__main__':
-    tf.app.run(main=main)
+    main()
 # end if
---
+++ softmax_1layer.py
@@ -0,0 +1,68 @@
+"""
+1 layer Neural network classifier using softmax activation.
+"""
+import os
+import numpy as np
+import tensorflow as tf
+
+def train(ds,
+          base_file,
+          weights_file,
+          input_dim,
+          output_dim,
+          learning_rate=0.3,
+          iterations=10000,
+          batch_size=100):
+    """
+    Trains
+    """
+    # Create the model
+    X = tf.placeholder(tf.float32, [None, input_dim])
+    W = tf.Variable(tf.zeros([input_dim, output_dim]))
+    B = tf.Variable(tf.zeros([output_dim]))
+    Y = tf.matmul(X, W) + B
+
+    # Original output
+    Y_ = tf.placeholder(tf.float32, [None, output_dim])
+
+    # Define loss / cross_entropy
+    cross_entropy = tf.reduce_mean(
+        tf.nn.softmax_cross_entropy_with_logits(labels=Y_, logits=Y))
+
+    # Defiine optimizer
+    train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+
+    # Accuracy measures
+    correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    # Init session
+    init = tf.global_variables_initializer()
+    sess = tf.InteractiveSession()
+    sess.run(init)
+    print()
+
+    # Training loop
+    pitstop = iterations // 25
+    for i in range(iterations):
+        batch_X, batch_Y = ds.train.next_batch(batch_size)
+        sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y})
+
+        if i % pitstop == 0:
+            a, c = sess.run([accuracy, cross_entropy],
+                            feed_dict={X: ds.train.images, Y_: ds.train.labels})
+            print('step', i, '|', 'accuracy:', a, 'loss:', c)
+        # end if
+    # end for
+    print()
+
+    # Test results
+    a, c = sess.run([accuracy, cross_entropy],
+                    feed_dict={X: ds.test.images, Y_: ds.test.labels})
+    print('Training Complete.', '|', 'Accuracy:', a, 'Loss:', c)
+
+    # Save the model
+    base, weight = sess.run([B, W])
+    np.save(base_file, base)
+    np.save(weights_file, weight)
+# end function
