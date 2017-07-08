ae63b1300aca943236f3e8717cc9e7bf40dfc590
dipu-bd <dipu.sudipta@gmail.com>
2017-07-08 06:30:58 +0600

    using array instead of string for letter and digits

--- config.py
+++ config.py
@@ -2,12 +2,62 @@
 Some configuration values used everywhere
 """
 
-# The numerals permitted in the vehicle registration plate : 
-NUMERALS = u"০১২৩৪৫৬৭৮৯"
+# The numerals permitted in the vehicle registration plate
+NUMERALS = [
+    u"০",
+    u"১",
+    u"২",
+    u"৩",
+    u"৪",
+    u"৫",
+    u"৬",
+    u"৭",
+    u"৮",
+    u"৯",
+]
 
-# The letters permitted in the vehicle registration plate 
-# + Appended some letters from DistrictLetters.py
-LETTERS = u"অইউএকখগঘঙচছজঝতথঢডটঠদধনপফবভমযরলশসহণষঞও"
+# The letters permitted in the vehicle registration plate
+# + appended some letters from district names
+LETTERS = [
+    u"অ",
+    u"ই",
+    u"উ",
+    u"এ",
+    u"ক",
+    u"খ",
+    u"গ",
+    u"ঘ",
+    u"ঙ",
+    u"চ",
+    u"ছ",
+    u"জ",
+    u"ঝ",
+    u"ত",
+    u"থ",
+    u"ঢ",
+    u"ড",
+    u"ট",
+    u"ঠ",
+    u"দ",
+    u"ধ",
+    u"ন",
+    u"প",
+    u"ফ",
+    u"ব",
+    u"ভ",
+    u"ম",
+    u"য",
+    u"র",
+    u"ল",
+    u"শ",
+    u"স",
+    u"হ",
+
+    u"ণ",
+    u"ষ",
+    u"ঞ",
+    u"ও",
+]
 
 # Reference https://en.wikipedia.org/wiki/Vehicle_registration_plates_of_Bangladesh
 
