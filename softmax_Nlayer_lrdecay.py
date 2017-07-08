56910132b0607aa56efac5ae37ddc0e247bdbd27
dipu-bd <dipu.sudipta@gmail.com>
2017-07-08 19:50:08 +0600

    introduced dropouts in softmax_Nlayer_lrdecay

--- softmax_Nlayer_lrdecay.py
+++ softmax_Nlayer_lrdecay.py
@@ -21,6 +21,9 @@ def train(ds,
 
     # variable learning rate
     lr = tf.placeholder(tf.float32)
+    # Probability of keeping a node during dropout
+    # = 1.0 at test time (no dropout) and 0.75 at training time
+    pkeep = tf.placeholder(tf.float32)
 
     # Build the model
     num_layers = len(layers)
@@ -28,12 +31,16 @@ def train(ds,
     W = [None] * num_layers  # weights
     Y = [None] * num_layers  # output
 
-    Y[0] = X
     for i in range(1, num_layers):
         # get random numbers between -0.2 and +0.2
         W[i] = tf.Variable(tf.truncated_normal([layers[i-1], layers[i]], stddev=0.1))
         B[i] = tf.Variable(tf.zeros([layers[i]]))
-        Y[i] = tf.nn.relu(tf.matmul(Y[i-1], W[i]) + B[i])
+    # end for
+
+    Y[0] = X
+    for i in range(1, num_layers - 1):
+        # output including dropouts
+        Y[i] = tf.nn.dropout(tf.nn.relu(tf.matmul(Y[i - 1], W[i]) + B[i]), pkeep)
     # end for
 
     Ylogits = tf.matmul(Y[-2], W[-1]) + B[-1]
@@ -77,11 +84,11 @@ def train(ds,
 
         # training step
         batch_X, batch_Y = ds.train.next_batch(batch_size)
-        sess.run(train_step, {X: batch_X, Y_: batch_Y, lr: learning_rate})
+        sess.run(train_step, {X: batch_X, Y_: batch_Y, lr: learning_rate, pkeep: 0.75})
 
         # print at each pitstop
         if i % pitstop == 0:
-            feed_dict = {X: ds.test.images, Y_: ds.test.labels}
+            feed_dict = {X: ds.test.images, Y_: ds.test.labels, pkeep: 1.0}
             a, c = sess.run([accuracy, cross_entropy], feed_dict)
             print("step %5d | accuracy = %6.2f%% | loss = %8.3f | LR = %f"
                   % (i, a * 100, c, learning_rate))
