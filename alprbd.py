e49dae81a93128e5ee6d810f745da0924739e7dc
dipu-bd <dipu.sudipta@gmail.com>
2017-07-10 04:55:40 +0600

    changed argument options a little

--- alprbd.py
+++ alprbd.py
@@ -10,24 +10,25 @@ def main():
     # Create a parser
     parser = ArgumentParser(description="ALPR-BD Command Line Utility: " + __version__)
 
-    # Positional argument
-    parser.add_argument(dest="input_image", \
-        help="Image of a car to recognize license plate number.")
-
     # Version information
     parser.add_argument("-v", "--version", action="version", \
         help="Displays the current version.", \
         version='%(prog)s ' + __version__)
 
+    # Positional argument
+    parser.add_argument(dest="input_image", \
+        help="Image of a car to recognize license plate number.")
+
     # Optional arguments
+    parser.add_argument("-e", "--extract", action="store_true", \
+        help="Extracts all detected plates in the current directory")
     parser.add_argument("-j", "--json", action="store_true", \
         help="Output in json format. Default=False")
-    parser.add_argument("-n", "--maxN", dest="number", default=10, type=int, \
+    parser.add_argument("-n", dest="top_n", default=10, type=int, \
         help="Maximum number of possible plate numbers. Default=10")
-    parser.add_argument("--mark", dest="output_file", \
+    parser.add_argument("-m", "--mark", action="store_true", \
         help="Highlight plate regions in the input image with most probable plate numbers")
-    parser.add_argument("--extract", action="store_true", \
-        help="Extracts all detected plates in current directory")
+    
     parser.add_argument("--debug", action="store_true", \
         help="Output debug information. Default=False")
 
