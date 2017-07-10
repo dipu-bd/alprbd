f6ef5d72f6986c844c77431c15266ebe929a0b73
dipu-bd <dipu.sudipta@gmail.com>
2017-07-10 21:38:17 +0600

    segment class finished

--- alprbd/models/segment.py
+++ alprbd/models/segment.py
@@ -8,7 +8,17 @@ class Segment:
     Single segment of plate number
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, id):
+        self._id = id       # segment id
+        self.guess = []     # list of (prediction, probability), in descending order
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def value(self):
+        """returns the top predicted value"""
+        return self.guess[0][0]
 
 # end class
