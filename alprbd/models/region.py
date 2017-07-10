3f7c6df300f0148f77a636bb352c60f4ef36caee
dipu-bd <dipu.sudipta@gmail.com>
2017-07-10 22:09:10 +0600

    basic implementation of region class

--- alprbd/models/region.py
+++ alprbd/models/region.py
@@ -8,7 +8,34 @@ class Region:
     Information about a region of interest
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, image, x, y, height, width):
+        """
+        Creates new instances
+        :param image: Image object this region belongs to
+        :param x: start position
+        :param y: start position
+        :param height: region height
+        :param width: region width
+        """
+        self._x1 = x
+        self._x2 = x + height
+        self._y1 = y
+        self._y2 = y + width
+        self._parent = image
+
+    @property
+    def image(self):
+        """extract the image of the region"""
+        return self._parent.image[self._x1:self._x2, self._y1:self._y2]
+
+    @property
+    def width(self):
+        """get width of the region"""
+        return self._y2 - self._y1
+
+    @property
+    def height(self):
+        """get height of the region"""
+        return self._x2 - self._x1
 
 # end class
