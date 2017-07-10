44f6f0a0a8e3528a8836be9cfb06473c47f341c3
dipu-bd <dipu.sudipta@gmail.com>
2017-07-10 20:15:27 +0600

    created test

---
+++ tests/test_image.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+from alprbd.models import Image
+
+class TestImage(TestCase):
+    def test_file(self):
+
+
+    def test_image(self):
+        self.fail()
+
+    def test_gray_image(self):
+        self.fail()
+
+    def test_save(self):
+        self.fail()
