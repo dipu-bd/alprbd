c9ef52e9dc4012519a75b40980fc5edc24666dd9
dipu-bd <dipu.sudipta@gmail.com>
2017-07-10 20:54:59 +0600

    improved tests

--- tests/image.py
+++ tests/image.py
@@ -1,19 +1,17 @@
+import numpy from np
 from alprbd.models.image import Image
 
 img = Image('samples/002.jpg')
 
+
 def file():
-    print(img.file)
+    assert img.file == 'samples/002.jpg'
 
 
 def image():
-    print(img.image)
-
-
-def gray():
-    print(img.gray)
+    assert np.array(img.image).shape[3] == 3
 
 
 def save():
-    pass
+    img.save()
 
