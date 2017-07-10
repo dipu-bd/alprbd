a817a4ebe99cdc2db4e4ddec37b2d94f104f7609
dipu-bd <dipu.sudipta@gmail.com>
2017-07-10 22:00:56 +0600

    segment class, reference to plate added

--- alprbd/models/segment.py
+++ alprbd/models/segment.py
@@ -8,17 +8,24 @@ class Segment:
     Single segment of plate number
     """
 
-    def __init__(self, id):
-        self._id = id       # segment id
-        self.guess = []     # list of (prediction, probability), in descending order
+    def __init__(self, plate, id):
+        self._id = id           # segment id
+        self._plate = plate     # plate
+        self.guess = []         # list of (prediction, probability), in descending order
 
     @property
     def id(self):
+        """id of the segment"""
         return self._id
 
     @property
     def value(self):
         """returns the top predicted value"""
         return self.guess[0][0]
+
+    @property
+    def plate(self):
+        """get the plate which this segment belongs to"""
+        return self._plate
 
 # end class
