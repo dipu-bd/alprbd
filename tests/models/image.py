034d10e93c515f3dd0b2526267261cf120b3f0a9
dipu-bd <dipu.sudipta@gmail.com>
2017-07-11 07:40:48 +0600

    image test updated

--- tests/models/image.py
+++ tests/models/image.py
@@ -6,10 +6,9 @@ class TestImage(TestCase):
     def test_all(self):
         img = alprbd.models.Image('../samples/002.jpg')
         assert img.file is not None
-        assert img.image.shape[2] == 3
+        assert img.original.shape[2] == 3
         assert img.height == 2448
         assert img.width == 3264
-        assert img.gray is None
         assert len(img.roi) == 0
         assert len(img.plates) == 0
         img.save()
