1efbfcc020b9e36b13ab55726f8646f89083409a
dipu-bd <dipu.sudipta@gmail.com>
2017-07-11 08:20:52 +0600

    made use of unittest functions

--- tests/worker/preprocess.py
+++ tests/worker/preprocess.py
@@ -3,17 +3,19 @@ import cv2
 import alprbd
 
 
-class TestImage(TestCase):
+class TestPreprocess(TestCase):
     def test_all(self):
+
         file = 'samples/002.jpg'
         image = alprbd.models.Image(file)
+        self.assertIsNotNone(image.original, msg="image load failed")
         alprbd.worker.preprocess.process(image)
-        assert image.gray is not None
-        assert len(image.gray.shape) == 2
-        assert image.scaled is not None
-        assert image.scaled.shape[0] == alprbd.config.SCALE_DIM[1]
-        assert image.scaled.shape[1] == alprbd.config.SCALE_DIM[0]
-        cv2.imshow("Gray Image", image.gray)
-        cv2.imshow("Scaled Image", image.scaled)
+        self.assertIsNotNone(image.gray, msg="no gray image")
+        self.assertEqual(len(image.gray.shape), 2, msg="gray is not 2 dimensional")
+        self.assertIsNotNone(image.scaled, msg="scaling failed")
+        self.assertEqual(image.scaled.shape[0], alprbd.config.SCALE_DIM[1], msg="scaled height mismatch")
+        self.assertEqual(image.scaled.shape[1], alprbd.config.SCALE_DIM[0], msg="scaled width mismatch")
+        cv2.imshow("preprocess test: gray image", image.gray)
+        cv2.imshow("preprocess test: scaled image", image.scaled)
         cv2.waitKey()
 
