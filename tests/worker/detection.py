b0a2fb3fa0d228f27134fe346f288b66322748ff
dipu-bd <dipu.sudipta@gmail.com>
2017-07-12 09:31:23 +0600

    test added for detection

---
+++ tests/worker/detection.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+import os
+import cv2
+import alprbd
+import numpy as np
+
+
+class TestDetection(TestCase):
+    def test_mixture_model(self):
+        kernel = alprbd.worker.detection.match_filter()
+        self.assertIsNotNone(kernel, msg="mixture model build failure")
+        #cv2.imshow('kernel: ', cv2.resize(kernel, (400, 150)))
+        #cv2.waitKey(5000)
+
+    def test_apply_matching(self):
+        #for f in np.sort(os.listdir('samples')):
+        f = '335.jpg'
+        file = os.path.join('samples', f)
+        image = alprbd.models.Image(file)
+        image = alprbd.worker.preprocess.process(image)
+        matched = alprbd.worker.detection.apply_matching(image.enhanced)
+        self.assertIsNotNone(matched, msg="mixture model build failure")
+        self.assertLessEqual(np.max(matched), 255)
+        self.assertGreaterEqual(np.min(matched), 0)
+        out = np.hstack((image.enhanced, matched))
+        cv2.imshow('matched: ' + f, out)
+        cv2.waitKey()
+
+    def test_regions(self):
+        #for f in np.sort(os.listdir('samples')):
+        f = '335.jpg'
+        file = os.path.join('samples', f)
+        image = alprbd.models.Image(file)
+        image = alprbd.worker.preprocess.process(image)
+        image = alprbd.worker.detection.detect_roi(image)
+        for region in image.roi:
+            cv2.imshow(f, cv2.resize(region.image, (350, 150)))
+            cv2.waitKey(500)
