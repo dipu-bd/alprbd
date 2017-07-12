89c28d1d08070421a5417834fcfcc3aed2caa9a9
dipu-bd <dipu.sudipta@gmail.com>
2017-07-12 11:12:12 +0600

    plate model: image property added

--- alprbd/models/plate.py
+++ alprbd/models/plate.py
@@ -8,11 +8,12 @@ class Plate:
     Information about detected plate
     """
 
-    def __init__(self, region):
+    def __init__(self, image, region):
         """
         Creates new instance of Plate
         :param region: the Region this plate originated from
         """
+        self._image = image     # formatted image of the plate
         self._region = region   # region this plate belongs to
         self.segments = []      # segments of the plate number
         self.guess = []         # list of (prediction, probability), in descending order
@@ -20,7 +21,12 @@ class Plate:
     @property
     def plate_image(self):
         """the image of the plate"""
-        return self._region.image()
+        return self._image
+
+    @property
+    def plate_region(self):
+        """the image of the plate"""
+        return self._region
 
     @property
     def value(self):
